15fa7b2ee6d57cbcfcc258ae055a9c63d049949b
csdmua <yuikatekim@gmail.com>
2017-04-06 13:34:36 +0700

    Update VuTienAnh.php

--- VuTienAnh.php
+++ VuTienAnh.php
@@ -1,7 +1,7 @@
 <?php
  
 /* == ID tài khoản muốn tăng share == */
-$user = 'shou.1807';
+$user = '100005990702045';
 /* == Token tài khoản chứa page == */
 $token = 'EAAAAAYsX7TsBANHumPxWquIiwK5qfTO0V49c3gSb7MLbeqOhHrUSH5Np09QKqxZBAF9pVuq46wShj1MFUCXfVSnLEC7o8ZBYYfDRaud9h6xi0Qmw1nc0yc8WQxIKGUngLjMV93eKScZAP7KzNDJZAOHtg8ZCQbg7xN7iW7fMUyy4n7ICZCoWFlQEiYl8eWoZA71v9Cb3ZBZBM5gZDZD';
 $accounts = json_decode(cURL('https://graph.facebook.com/me/accounts?access_token=' . $token),true);
